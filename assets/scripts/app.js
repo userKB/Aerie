6b7405559eceb051c1a98ba9cbb65a343dfbddca
kenneth kirui <kenbett@live.com>
2017-06-20 01:40:31 +0300

    code structure
    where all the functionality and styling will go

---
+++ assets/scripts/app.js
@@ -0,0 +1,64 @@
+/*EXTERNAL LIBRARIES
+=============================
+*/
+
+
+/*HELPER FUNCTIONS
+*===============================
+*/
+
+/*HELPER VARIABLES*/
+
+/*JQUERY HELPER*/
+
+/*BREAKPOINTS
+*===========================
+*/
+
+/*PROJECTS NAVIGATION
+when going from one project to another
+*=======================================
+*/
+
+
+/*WebGL SCENE
+*=====================
+*/
+
+/*ANIMATIONS & SCENE TRANSITIONS
+TIMELINES
+*======================================
+*/
+
+/*SECTIONS
+*==========================
+*/
+
+/*SECTION ROUTING
+*===========================
+	-Page transitions	
+*/
+
+/*MOUSE EVENTS
+*===========================
+*/
+
+/*MOUSE MOVE INTERACTING WITH THE SCENE
+*============================================
+*/
+
+/*DEVICE ORIENTATION EVENTS
+*==============================
+*/
+
+
+
+
+
+
+
+
+
+
+
+
